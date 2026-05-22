8e8cebce197a73ff58a82a58a086719fc9a3df30
János Illés <ijanos@gmail.com>
2026-05-22 08:43:19 +0200

    Change the cube to an extruded A glyph

--- src/geometry/glyph.ts
+++ src/geometry/glyph.ts
@@ -1,9 +1,30 @@
-import type { ManifoldToplevel, Mesh } from 'manifold-3d';
+import type { ManifoldToplevel, Mesh, Polygons, FillRule, SimplePolygon } from 'manifold-3d';
 import { getManifold } from './manifold';
 
 export function createTestSolid(): Mesh {
   const manifold: ManifoldToplevel = getManifold();
-  const solid = manifold.Manifold.cube([100, 100, 100], true);
+
+  const outerA: SimplePolygon = [
+    [0, 0],
+    [18, 0],
+    [30, 28],
+    [70, 28],
+    [82, 0],
+    [100, 0],
+    [62, 120],
+    [38, 120],
+  ];
+
+  const innerCutout: SimplePolygon = [
+    [40, 48],
+    [60, 48],
+    [50, 82],
+  ];
+
+  const contours: Polygons = [outerA, innerCutout];
+  const fillRule: FillRule = 'EvenOdd';
+  const crossSection = manifold.CrossSection.ofPolygons(contours, fillRule);
+  const solid = crossSection.extrude(150, 0, 0, [1, 1], true);
 
   return solid.getMesh();
 }
